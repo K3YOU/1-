49b5fecff39d745521e5e8de15559b9f48b46085
K3YOU <7592667@naver.com>
2023-01-12 17:32:57 +0900

    0112 진짜 마지막!!!!!!!!

--- router/cartct.js
+++ router/cartct.js
@@ -1,13 +1,13 @@
 // carNo 최대값 구하기
-// function getMaxCountNo(cartDB) {
-//     var maxNo = 0;
-//     for(var i=0; i<cartDB.length; i++) {
-//         if(maxNo < cartDB[i]["cartNo"]) {
-//             maxNo = cartDB[i]["cartNo"];
-//         }
-//     }
-//     return maxNo;
-// }
+function getMaxCountNo(cartDB) {
+    var maxNo = 0;
+    for(var i=0; i<cartDB.length; i++) {
+        if(maxNo < cartDB[i]["cartNo"]) {
+            maxNo = cartDB[i]["cartNo"];
+        }
+    }
+    return maxNo;
+}
 
 // 내 장바구니 총 결제 금액 구하기
 // function getMyCartTotal(cartDB, log) {
@@ -21,90 +21,105 @@
 //     return total;
 // }
 
-module.exports = function(app){
-     // 장바구니 등록 페이지
-    // app.get("/cart", function(req, res){ 
-        
-    //     var log = req.session.log;
+// module.exports = function(app,prdData){
+//      // 장바구니 등록 페이지
+//         //상세페이지 -> 키 값별로 페이지 내용 바뀌게
+//         var testArray = [];
 
-        //회원가입이 안 될 때,
-    //     if(log != null) {
-    //         res.redirect("memberLogin");
-    //     } else {
-
-    //         var cartDB = req.session.cartDB;
-
-    //         var cartNo = Number(getMaxCountNo(cartDB)) + 1;
-    //         var cartMemberId = log;
-    //         var cartPrdName = req.query.PrdName;
-    //         var cartBuyCount = Number(req.query.cartbuyCount);
-    //         var cartPrdImage = req.query.prdImage;
-    //         var cartBuyPrdPrice = Number(req.query.prdPrice);
-    //         var cartdscntRate = Number(req.qu)
+//         app.get("/cartPro",function(req,res){
+//             var key = req.query.key;
+//             res.redirect("cart")
+//         })
 
 
-    //         var cart = 
-    //             {"cartNo" : cartNo, 
-    //             "cartMemberId" : cartMemberId, 
-    //             "cartPrdName" : cartPrdName,
-    //             "cartPrdImg" : cartPrdImg,
-    //             "cartbrndName" : cartbrndName  , 
-    //             "cartsaleOrNot " : true, 
-    //             "cartBuyCount" :cartBuyCount , 
-    //             "cartPrdImage" : artPrdImage, 
-    //             "cartdscntRate": cartdscntRate,
-    //             "cartBuyPrdPrice" : cartBuyPrdPrice  , 
-    //             "cartsalesQuantity":cartsalesQuantity }
+//         // app.get("/cart?key="+prdData, function(req, res){ 
+//         //     var log = req.session.log;
             
+//         //     testArray.push(prdData);
 
-    //         cartDB.push(cart);
+//         //     // 회원가입이 안 될 때,
+//         //     if(log != null) {
+//         //         return res.redirect("memberLogin");
+//         //     } else {
+//         //         console.log(testArray, "testArray")
+//         //         var cartDB = req.session.cartDB;
+//         //         var cartNo = Number(getMaxCountNo(cartDB)) + 1;
+//         //         var cartMemberId = log;
+//         //         var cartPrdName = req.query.PrdName;
+//         //         var cartBuyCount = Number(req.query.cartbuyCount);
+//         //         var cartPrdImage = req.query.prdImage;
+//         //         var cartBuyPrdPrice = Number(req.query.prdPrice);
+//         //         var cartdscntRate = Number(req.query.cartdscntRate)
+//         //         var cartkey = req.query.key;
+//         //         var cartsalesQuantity = Number(req.query.salesQuantity)
+                
 
-    //         res.redirect("cart");
-    //     }
-        
-    // });
+//         //         var cart = 
+//         //             {"cartNo" : cartNo, 
+//         //             "cartMemberId" : cartMemberId, 
+//         //             "cartPrdName" : cartPrdName,
+//         //             "cartPrdImage" : cartPrdImage,
+//         //             // "cartbrndName" : cartbrndName  , 
+//         //             "cartsaleOrNot " : true, 
+//         //             "cartBuyCount" :cartBuyCount , 
+//         //             "cartPrdImage" : cartPrdImage, 
+//         //             "cartdscntRate": cartdscntRate,
+                    
+//         //             "cartBuyPrdPrice" : cartBuyPrdPrice  , 
+//         //             "cartsalesQuantity":cartsalesQuantity,
+                   
+//         //         }
+                
+    
+//         //         cartDB.push(cart);
 
-    // 장바구니 확인 페이지
-    // app.get("/cartInfo", function(req, res){
-    //     var log = req.session.log;
-    //     var cartDB = req.session.cartDB;
-    //     var name = req.session.name;
-
-
-    //     if(log == null) {
-    //         res.redirect("memberLogin");
-    //     } else {
-
-    //         var total = getMyCartTotal(cartDB, log);
-    //         var renderData = {	
-    //             "log" : log,
-    //             "name" : name,
-    //             "cartDB" : cartDB,
-    //             "total" : total
-    //         };
-    //         res.render("cart.ejs", renderData); 
-    //     }
-    // });
-
-
-    // 장바구니 삭제
-    // app.get("/deletePro", function(req, res){
-    //     // var cartDB = req.session.cartDB;
-    //     // var cartNo = req.query.cartNo;
-    //     cDB = [1];
-    //     req.session.scoreDB = cDB
-    //     var index  = req.query.index
-    //     // var index = 0;
-    //     // for(var i=0; i<cartDB.length; i++) {
-    //     //     if(cartDB[i]["cartNo"] == cartNo) {
-    //     //         index = i;
-    //     //         break;
-    //     //     }
-    //     // }
-    //     cDB.splice(index, 1);
-
-    //     res.redirect("cart"); 
-    // });
+//         //        return res.redirect("cart");
+//         //     }
+            
+//         // });
 
 
-};
+//     // 장바구니 확인 페이지
+//     // app.get("/cartInfo", function(req, res){
+//     //     var log = req.session.log;
+//     //     var cartDB = req.session.cartDB;
+//     //     var name = req.session.name;
+
+
+//     //     if(log == null) {
+//     //         res.redirect("memberLogin");
+//     //     } else {
+
+//     //         var total = getMyCartTotal(cartDB, log);
+//     //         var renderData = {	
+//     //             "log" : log,
+//     //             "name" : name,
+//     //             "cartDB" : cartDB,
+//     //             "total" : total
+//     //         };
+//     //         res.render("cart.ejs", renderData); 
+//     //     }
+//     // });
+
+
+//     // 장바구니 삭제
+//     // app.get("/deletePro", function(req, res){
+//     //     // var cartDB = req.session.cartDB;
+//     //     // var cartNo = req.query.cartNo;
+//     //     cartDB = [1];
+//     //     req.session.scoreDB = cDB
+//     //     var index  = req.query.index
+//     //     // var index = 0;
+//     //     // for(var i=0; i<cartDB.length; i++) {
+//     //     //     if(cartDB[i]["cartNo"] == cartNo) {
+//     //     //         index = i;
+//     //     //         break;
+//     //     //     }
+//     //     // }
+//     //     cDB.splice(index, 1);
+
+//     //     res.redirect("cart"); 
+//     // });
+
+
+// };
